436b346838571c293ed15a64f024a65c899558ce
NeoNeoHo <b95042@gmail.com>
2019-06-22 02:25:01 +0800

    update

--- client/app/Factory/Cart/Cart.service.js
+++ client/app/Factory/Cart/Cart.service.js
@@ -247,18 +247,18 @@ angular.module('webApp')
 			// 玫瑰角鯊植萃護手霜，product_id: 368
 			// 薰衣草植萃護手霜，product_id: 467
 
-			Product.getGifts([272, 421]).then(function(gifts) {
-				lcart.giftWithPurchase = [];
-				if(price_after_discount >= 450) {
-					if(gifts[0].quantity > 1) {
-						lcart.giftWithPurchase.push(_mapGoodFormGift(gifts[0]));
-					}			
-				}
-				if(price_after_discount >= 600) {	
-					if(gifts[1].quantity > 1) {
-						lcart.giftWithPurchase.push(_mapGoodFormGift(gifts[1]));
-					}			
-				}
+			// Product.getGifts([272, 421]).then(function(gifts) {
+			// 	lcart.giftWithPurchase = [];
+			// 	if(price_after_discount >= 450) {
+			// 		if(gifts[0].quantity > 1) {
+			// 			lcart.giftWithPurchase.push(_mapGoodFormGift(gifts[0]));
+			// 		}			
+			// 	}
+			// 	if(price_after_discount >= 600) {	
+			// 		if(gifts[1].quantity > 1) {
+			// 			lcart.giftWithPurchase.push(_mapGoodFormGift(gifts[1]));
+			// 		}			
+			// 	}
 				// if(price_after_discount >= 800) {
 				// 	if(gifts[2].quantity > 1) {
 				// 		lcart.giftWithPurchase.push(_mapGoodFormGift(gifts[2]));
@@ -282,11 +282,11 @@ angular.module('webApp')
 				// 		lcart.giftWithPurchase.push(_mapGoodFormGift(gifts[4]));
 				// 	}			
 				// }			
-				defer.resolve(lcart);				
-			}, function(err) {
-				defer.resolve(lcart);
-			});
-			// defer.resolve(lcart);
+			// 	defer.resolve(lcart);				
+			// }, function(err) {
+			// 	defer.resolve(lcart);
+			// });
+			defer.resolve(lcart);
 			return defer.promise;
 		}
 
